6a787179f7d2e39327e91d32b1e579315d9e4e0c
tsukasakaiko <kaikobt825@gmail.com>
2021-12-29 11:28:46 +0900

    room_usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_154538) do
+ActiveRecord::Schema.define(version: 2021_12_28_156538) do
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
